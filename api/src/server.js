a3e25ed6695e91ad5936d7ed7a895c28522a2b15
Gerardo Leal <gleal@atlassian.com>
2021-06-13 22:12:46 +1000

    API resolver and schema

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,14 @@
-const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers')
-const {models, db} = require('./db')
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const { models, db } = require("./db/index");
 
-const server = new ApolloServer()
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { models },
+});
 
-server.listen().then(({ url }) => {
+server.listen(4000).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
-})
+});
